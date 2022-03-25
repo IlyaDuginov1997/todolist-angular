bec447a6e35dd52ee5a0b68676d8eb53c9e63590
IlyaWork <ilyaduginov1997@gmail.com>
2022-03-25 17:22:43 +0300

    sending params

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+export type Post = {
+  name: string
+  text: string
+  id: number
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'todolist';
+  title = 'todolist'
+
+  posts: Post[] = [
+    {name: 'Ilya', text: 'I want to have a work', id: 1},
+    {name: 'Artem', text: 'I like programming process with React', id: 2},
+  ]
 }
